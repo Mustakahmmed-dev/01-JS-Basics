6a12a7e7e7533deeef866e146d680ef02122af68
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-06 11:12:58 +0600

    comparison with logical and other operators

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
 
 
-        // Variables declaration keywords: var, let, const
-        // Comparison rel keywords: ==, >, <, !=, ||, &&, +=, -=, *=, */,
+// Variables declaration keywords: var, let, const
+// Comparison rel keywords: ==, >, <, !=, ||, &&, +=, -=, *=, */,
 
+// User validation
+var name = "Mustak";
+var age = 20;
+var country = "Arakan";
 
-        var name = "Mustak";
-        var age = 20;
-        var country = "Arakan";
-        
-        if(name == "Mustak" && age == 20 && country == "Arakan"){
-            console.log("Yes, this is true that it's Mustak Ahmmed from Arakan");
-        }
-        else{
-            console.log("No, maybe you are someone not relevant to the given criteria");
-        }
- 
+if (name == "Mustak" && age == 20 && country == "Arakan") {
+    console.log("Yes, this is true that it's Mustak Ahmmed from Arakan");
+}
+else {
+    console.log("No, maybe you are someone not relevant to the given criteria");
+}
+
+// Comparison
+let studentName = "Ahmmed";
+let studentSubject = "Mathematics";
+let studentMarks = 80;
+let studentRole = 1;
+
+if(studentSubject == "Mathematics" && studentMarks >= 80 && studentRole >= 3){
+    console.log("This student has a good capacity to survive this world");
+
+}
+else{
+    console.log("This student should try to learn and pay attention more in the learning and disciplines");
+}
