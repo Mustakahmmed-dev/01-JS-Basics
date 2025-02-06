e8015dc90afaf6eb1f7474c3dabfae8434c51fe3
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-06 21:34:10 +0600

    Ticketing allowance validation

--- script.js
+++ script.js
@@ -9,22 +9,35 @@ var age = 20;
 var country = "Arakan";
 
 if (name == "Mustak" && age == 20 && country == "Arakan") {
-    console.log("Yes, this is true that it's Mustak Ahmmed from Arakan");
+    // console.log("Yes, this is true that it's Mustak Ahmmed from Arakan");
 }
 else {
-    console.log("No, maybe you are someone not relevant to the given criteria");
+    // console.log("No, maybe you are someone not relevant to the given criteria");
 }
 
-// Comparison
+// Comparison & Conditions
 let studentName = "Ahmmed";
 let studentSubject = "Mathematics";
 let studentMarks = 80;
 let studentRole = 1;
 
 if(studentSubject == "Mathematics" && studentMarks >= 80 && studentRole >= 3){
-    console.log("This student has a good capacity to survive this world");
+    // console.log("This student has a good capacity to survive this world");
 
 }
 else{
-    console.log("This student should try to learn and pay attention more in the learning and disciplines");
+    // console.log("This student should try to learn and pay attention more in the learning and disciplines");
+}
+
+// Ticketing allowance
+let customer = "Jackie";
+let customerAge = 12;
+let freedomF = true;
+
+if(customerAge >= 18 || freedomF === true){
+    console.log("Yes, please" + " " + customer + " You can enter the museum");
+}
+else{
+    console.log("Sorry " + customer + ", You can't enter!");
+
 }
