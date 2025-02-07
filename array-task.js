801f71cf372876041e9bb15be4942eb96a1684e3
Mustak Ahmmed <mustakahmmed793@gmail.com>
2025-02-07 22:29:30 +0600

    Array with items, array for addition and removable of elements in the array

---
+++ array-task.js
@@ -0,0 +1,13 @@
+// Five items and console log the 3rd index element
+let fruits = ["apple", "mango", "banana", "orange", "grape"];
+console.log("The fruit of third element " + fruits[2])
+
+// Add and remove elements
+let touristDestination = ["Buthidaung", "Rathidaung", "Kyataw"];
+touristDestination.push("Ramre");
+touristDestination.push("Minbya", "Mrauk U");
+
+touristDestination.pop();
+
+console.log("Tourist destinations " + touristDestination);
+
